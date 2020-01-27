8ed3671c6b1680b5e05a47e0b9182585fbc25951
Christian Adamsson <Christian.liu171@gmail.com>
2020-01-27 21:52:17 +0100

    christians fix

--- client.js
+++ client.js
@@ -1,3 +1,5 @@
+var X = 5;
+
 displayView = function(){
 // the code required to display a view
 };
@@ -5,5 +7,39 @@ window.onload = function(){
 //code that is executed as the page is loaded.
 //You shall put your own custom code here.
 //window.alert() is not allowed to be used in your implementation.
-window.alert("Hello TDDD97!");
-};
+document.getElementById('containerdiv').innerHTML = document.getElementById("welcomeview").innerHTML;
+
+}
+//todo, fix alert message to not be alert. maybe css and make the fields red
+function checkpassword(){
+  var loginpassword = document.getElementById('loginpass');
+
+  if(loginpassword.value.length < X){
+    loginpassword.style.color = "red";
+    alert("för kort lösenord");
+    return false;
+  }
+  else{
+   return true;
+  }
+}
+function checksamepass(){
+  var pw1 = document.getElementById('pwd1');
+  var pw2 = document.getElementById('pwd2');
+  if(pw1.value.length < X){
+    alert("ditt lösenord är för kort");
+    pw1.style.color = "red";
+    return false;
+  }
+  if(pw1.value != pw2.value){
+    alert("dem stämmer inte överrens")
+    pw2.style.color = "red";
+    return false;
+  }
+  else{
+    return true;
+    //step 5
+    //serverstub.signUp(document.getElementById('signupform'));
+  }
+
+}
