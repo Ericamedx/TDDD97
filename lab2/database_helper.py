51580900a03d88a49ce7786f243e3e8b457a5932
-' Erica ahlqvist <ericaahlqvist@hotmail.com>
2020-02-21 16:29:54 +0100

    Börjat med connect och finduser

--- lab2/database_helper.py
+++ lab2/database_helper.py
@@ -3,3 +3,38 @@
 # domain functions like signin or signup and shall only contain data-centric functionality like
 # find_user(), remove_user(), create_post() and … . E.g. Implementing sign_in() in server.py shall
 # involve a call to find_user() implemented in database_helper.py .
+
+import sqlite3
+
+# inspo https://docs.python.org/2/library/sqlite3.html
+def connect_db():
+    conn = sqlite3.connect('database.db')
+    c = conn.cursor() # skapa cursor object som sedan används för att kalla på execute()
+
+    # lägg till data genom execute
+    c.execute(""" CREATE TABLE IF IT DOES NOT EXIST user(
+                Email TEXT PRIMARY KEY,
+                Password TEXT,
+                Firstname TEXT,
+                Familyname TEXT,
+                Gender TEXT,
+                City TEXT,
+                Country TEXT)""")
+
+    # behövs mer sånna tabeller?
+
+    conn.commit() # spara ändringar
+    conn.close() # stäng kontakten
+    return conn
+
+
+def find_user(email): # kolla om användaren finns
+    #connect_db()
+    db = connect_db()
+    curs = db.cursor()
+    curs.execute(""" SELECT Email, Password FROM user WHERE Email = ? """, (email,))
+
+    if curs.fetchall(): #oklart med denna..
+        print('welcome')
+    else:
+        print('log in failed')
--- lab2/server.py
+++ lab2/server.py
@@ -4,7 +4,20 @@ from flask import Flask, escape, request
 app = Flask(__name__)
 
 #steg 0 - hello world kolla mot postman?
+#steg 0 - hello world kolla mot postman
 @app.route('/')
 def hello():
     name = request.args.get("name", "world")
     return f'Hello, {escape(name)}!'
+
+@app.route('/sign_in', methods = ["POST"])
+def sign_in():
+    #logga in användare
+    email = request.form['email'] #eller username
+    password = request.form['password']
+    user_info = database_helper.find_user(email)
+
+    if user_info and user_info["email"] == email and user_info["password"] == password:
+        # returnera token och succes = true
+    else:
+        # returnera att det är fel namn samt succes = false
